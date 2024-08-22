5efa1bac42750e1199ad6d8d5c76accf6f8331d8
TJ2021 <74563536+TJ2021@users.noreply.github.com>
2024-08-22 07:52:40 -0500

    Update settings.py

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
